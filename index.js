8e7b916ecf30c55c886058e6c4e1562b7d7d488a
gasnicowydivolper <apkisix123@gmail.com>
2024-04-01 11:26:15 +0200

    dodanie autoupdate
    2.1.5

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // backend by gasnic https://github.com/OLOMIK
-const { app, BrowserWindow } = require('electron')
-
+const { app, BrowserWindow, Notification } = require('electron')
+var fs = require('fs');
+const https = require('https');
+const url = 'https://crystalx.pl/colorbj/lts-version.txt';
 const createWindow = () => {
   const win = new BrowserWindow({autoHideMenuBar: true, icon: "niepaint.ico"})
   win.maximize()
@@ -10,4 +12,49 @@ const createWindow = () => {
 app.whenReady().then(() => {
   createWindow()
   console.log("gui uruchomione poprawnie")
+  checkForUpdates();
 })
+
+
+
+// tu autoupdate robimy
+function checkForUpdates() {
+  https.get(url, (res) => {
+    let data = '';
+
+    
+    res.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    
+    res.on('end', () => {
+      const remoteVersion = data.trim();
+      compareVersions(remoteVersion);
+    });
+  }).on("error", (err) => {
+    console.error("Error: " + err.message);
+  });
+}
+function compareVersions(remoteVersion) {
+  const packageJson = require('./package.json');
+  const localVersion = packageJson.version;
+
+  if (remoteVersion > localVersion) {
+    showUpdateNotification(remoteVersion);
+  }
+}
+function showUpdateNotification(rv) {
+  const notification = new Notification({
+    title: 'Dostępna aktualizacja',
+    body: 'Jest dostępna nowa wersja ColorBJ. Kliknij aby pobrać. Wersja: ' + rv,
+    icon: 'niepaint.png'
+  });
+
+  notification.onclick = () => {
+    require("shell").openExternal("https://github.com/OLOMIK/ColorBJ/")
+    
+  };
+
+  notification.show();
+}
