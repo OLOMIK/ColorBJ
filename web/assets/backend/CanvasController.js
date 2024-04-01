dac1ef0aa02a7d0018c7f73d399734a18b73445c
gasnicowydivolper <apkisix123@gmail.com>
2024-04-01 11:40:49 +0200

    2.2 version
    auto-update, responsywność

--- web/assets/backend/CanvasController.js
+++ web/assets/backend/CanvasController.js
@@ -1,18 +1,32 @@
 // backend by gasnic https://github.com/OLOMIK
-
+console.log("załadowano backend")
 window.onload = setupCanvas;
 
 var painting = false;
 var kolor = "#5e5e5e";
 var canvas, ctx;
-var canvasWidth = 1400;
-var canvasHeight = 840;
-var blockSize = 5; 
-var currentBackgroundColor = "#111111"; 
+var blockSize = 5;
+var currentBackgroundColor = "#111111";
 var rubberMode = false;
-function setBlockSize(size){
+
+function setCanvasSize() {
+    if (window.innerWidth < 768) { 
+        canvasWidth = window.innerWidth - 70; 
+        canvasHeight = 600; 
+    } else {
+        canvasWidth = 1200; 
+        canvasHeight = 800; 
+    }
+    canvas.width = canvasWidth;
+    canvas.height = canvasHeight;
+    ctx.fillStyle = currentBackgroundColor;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+function setBlockSize(size) {
     blockSize = size;
 }
+
 document.getElementById("wyb").addEventListener('change', function() {
     ustawkolor(this.value);
 });
@@ -21,35 +35,38 @@ function setupCanvas() {
     canvas = document.getElementById('niepaintCanvas');
     if (canvas) {
         ctx = canvas.getContext('2d');
-
-        canvas.width = canvasWidth;
-        canvas.height = canvasHeight;
-
-       
-        ctx.fillStyle = currentBackgroundColor;
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        setCanvasSize();
+        window.addEventListener('resize', setCanvasSize);
 
         canvas.addEventListener('mousedown', startPainting);
         canvas.addEventListener('mousemove', paint);
         canvas.addEventListener('mouseup', stopPainting);
         canvas.addEventListener('mouseleave', stopPainting);
 
-        canvas.addEventListener('touchstart', startPainting, {passive: false});
-        canvas.addEventListener('touchmove', touchPaint, {passive: false});
-        canvas.addEventListener('touchend', stopPainting, {passive: false});
+        canvas.addEventListener('touchstart', startPainting, {
+            passive: false
+        });
+        canvas.addEventListener('touchmove', touchPaint, {
+            passive: false
+        });
+        canvas.addEventListener('touchend', stopPainting, {
+            passive: false
+        });
     } else {
         console.error('Nie znaleziono elementu canvas!');
+        console.log("wystąpił błąd podczas ładowania edytora")
     }
 }
-function ustawkolor(color){
-    if(color == '#000000'){
+
+function ustawkolor(color) {
+    if (color == '#000000') {
         kolor = '#5e5e5e'
-    }else{
+    } else {
         kolor = color;
         painting = false;
     }
-    
 }
+
 function startPainting(event) {
     painting = true;
     paint(event);
@@ -69,16 +86,13 @@ function paint(event) {
         x = event.touches[0].pageX - canvas.offsetLeft;
         y = event.touches[0].pageY - canvas.offsetTop;
     }
-    if(rubberMode) {
-        ctx.fillStyle = currentBackgroundColor 
+    if (rubberMode) {
+        ctx.fillStyle = currentBackgroundColor
         ctx.fillRect(x, y, blockSize, blockSize);
-    }else{
+    } else {
         ctx.fillStyle = kolor;
         ctx.fillRect(x, y, blockSize, blockSize);
     }
-    
-
-
 }
 
 function touchPaint(e) {
@@ -88,13 +102,12 @@ function touchPaint(e) {
 
 function toggleRubberMode() {
     rubberMode = !rubberMode;
-    
 }
+
 function disableRubberMode() {
     rubberMode = false;
-    
-    
 }
+
 function fillSzachownica() {
     kolor = document.getElementById("colorSel").value;
     var canvas = document.getElementById("niepaintCanvas");
@@ -105,6 +118,7 @@ function fillSzachownica() {
     container.style.display = 'none';
     painting = false;
 }
+
 function clearSzachownica() {
     var canvas = document.getElementById("niepaintCanvas");
     var ctx = canvas.getContext("2d");
@@ -115,16 +129,14 @@ function clearSzachownica() {
 }
 
 function saveImage() {
-   var filename = document.getElementById('filenameInput').value.trim();
-   var canvas = document.getElementById("niepaintCanvas");
-   var link = document.createElement('a');
-   link.download = filename;  
-   link.href = canvas.toDataURL();
-   link.click();
+    var filename = document.getElementById('filenameInput').value.trim();
+    var canvas = document.getElementById("niepaintCanvas");
+    var link = document.createElement('a');
+    link.download = filename;
+    link.href = canvas.toDataURL();
+    link.click();
 }
 
-
-  
 function openImage(event) {
     var input = event.target;
     var reader = new FileReader();
@@ -138,20 +150,19 @@ function openImage(event) {
         image.src = dataURL;
     };
     reader.readAsDataURL(input.files[0]);
-}   
+}
+
 function drawTextOnCanvas(text, x, y, font, color) {
     const canvas = document.getElementById('niepaintCanvas');
     const ctx = canvas.getContext('2d');
-    
+
     ctx.font = font || 'Arial';
     ctx.fillStyle = color || 'black';
     ctx.fillText(text, x, y);
 }
-    
 
-function dupkadupeczka(){
-    
-    drawTextOnCanvas(document.getElementById("tekscior").value, document.getElementById("posx").value, document.getElementById("posy").value, document.getElementById("rozmiar").value + "px " + document.getElementById("fontname").value , document.getElementById("kolor").value)
+function dupkadupeczka() {
+    drawTextOnCanvas(document.getElementById("tekscior").value, document.getElementById("posx").value, document.getElementById("posy").value, document.getElementById("rozmiar").value + "px " + document.getElementById("fontname").value, document.getElementById("kolor").value)
     const container = document.getElementById('windowContainer7');
     container.style.display = 'block';
 }
