7dc0b476abb66cf685c79a32d6bee1a33a258c70
gasnicowydivolper (OLOMIK) <apkisix123@gmail.com>
2025-02-17 20:02:44 +0100

    markdown

--- build.js
+++ build.js
@@ -1,11 +1,14 @@
 const { execSync } = require('child_process');
 const fs = require('fs');
+
 const packageJson = JSON.parse(fs.readFileSync('package.json', 'utf8'));
 const version = packageJson.version;
+
 const commands = [
-  `electron-packager . ColorBJ-${version} --platform=linux --arch=x64 --icon=niepaint.png`,
-  `electron-packager . ColorBJ-${version} --platform=win32 --arch=x64 --icon=niepaint.ico`
+  `npx electron-packager . ColorBJ-${version} --platform=linux --arch=x64 --icon=niepaint.png`,
+  `npx electron-packager . ColorBJ-${version} --platform=win32 --arch=x64 --icon=niepaint.ico`
 ];
+
 commands.forEach(cmd => {
   console.log(`Running: ${cmd}`);
   execSync(cmd, { stdio: 'inherit' });
